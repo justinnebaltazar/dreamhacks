6610b8889315623d7b8227e74c0062f5859c6c87
maruyaya <115956361+maruyaya@users.noreply.github.com>
2025-03-15 11:07:16 -0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
 export const Navbar = () => {
     return (
-        <div>
-            <h1>Welcome to REM News</h1>
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <h1>REM News</h1>
+            </div>
+            <div className={styles.right}>
+                <p>News Feed</p>
+                <p>About</p>
+                <p>Log In</p>
+                <p>Sign Up</p>
+            </div>
         </div>
+        
     )
 }
