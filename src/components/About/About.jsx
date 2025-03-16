c9add3417f09cfe33888c8da71ae3c96b669f88a
cjb22 <cjb22@sfu.ca>
2025-03-15 17:09:08 -0700

    Grammar changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,14 +9,13 @@ export const About = () => {
                   
                 <div className = {styles.textContainer}>
                 <h1>About:</h1>
-                    <p>
-                    REM news came about thanks to a generous donation from SBC (Sleep Board of Canada).   </p>
-                    
-                    <p>REM news sources our news directly from the dreams of Canadians through proprietary 
-                    software that unknowingly scans the Brain waves of Canadians as they sleep. </p>
-                    
-                    <p>Critics of our work have claimed that REM news's practices are "invasive" and "illegal", to them we say we know what you were dreaming about last night and we will release it to the public. </p>
-                    
+                 <p>
+ • REM News came about thanks to a generous donation from SBC (Sleep Board of Canada).   </p>
+                    
+                    <p> •REM News sources our news directly from the dreams of Canadians through proprietary 
+ software that unknowingly scans the Brain waves of Canadians as they sleep. </p>
+                    
+                    <p>• Critics of our work have claimed that REM news's practices are "invasive" and "illegal" to them, we say: "We know what you were dreaming about last night, and we will release it to the public". </p>
                     </div>
 
                 <div className = {styles.Quote}>
